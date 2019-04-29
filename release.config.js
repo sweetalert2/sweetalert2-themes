920f28c399ba35c50f8cc0fad2905af7e14ce07b
Limon Monte <limon.monte@gmail.com>
2019-04-29 10:08:58 +0300

    Automatically publish all themes

--- release.config.js
+++ release.config.js
@@ -7,6 +7,10 @@ module.exports = {
     '@semantic-release/github',
   ],
   prepare: [
+    {
+      'path': '@semantic-release/exec',
+      'cmd': 'VERSION=${nextRelease.version} node tools/publish-all-themes'
+    },
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
